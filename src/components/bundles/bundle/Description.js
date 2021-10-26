2f18e71eff78b4ca8aee844a078e6226f45dba7a
Adam Kudłacik <adam.kud@wp.pl>
2021-10-27 00:08:13 +0200

    save

--- src/components/bundles/bundle/Description.js
+++ src/components/bundles/bundle/Description.js
@@ -1,8 +1,37 @@
-import React from "react";
+/* eslint-disable */ 
+import { gql, useMutation } from "@apollo/client";
+import React, { useState } from "react";
+
+const UPDATE_DESCRIPTION = gql`
+  mutation updateDescription(
+    $id: MongoID!
+    $record: UpdateByIdTagBundleInput!
+  ) {
+    tagBundleUpdateById(_id: $id, record: $record) {
+      recordId
+    }
+  }
+`;
 
 function Description({ _id, creatorId, description }) {
-  // const [description, setDescription] = useState(description);
+  const [descriptionToChange, setDescriptionToChange] = useState(description);
+  // { description: "To jest nowe descrtion" }
+
+  const [updateDescription] = useMutation(UPDATE_DESCRIPTION, {
+    variables: { description: "To jest nowe description" },
+  });
+  
   const userId = "61671921b7efc009eaf79450";
+
+  const handleDescriptionChange = () => {
+    console.log('updated')
+    updateDescription({
+      variables: {
+        id: _id,
+        record: {description: descriptionToChange},
+      },
+    });
+  };
 
   return (
     <div>
@@ -10,6 +39,12 @@ function Description({ _id, creatorId, description }) {
       <p>UserId: {userId}</p>
       <p>CreatorId: {creatorId}</p>
       <p>description: {description}</p>
+      <textarea
+        value={descriptionToChange}
+        onChange={(e) => setDescriptionToChange(e.target.value)}
+      />
+
+      <button onClick={handleDescriptionChange}>Update</button>
     </div>
   );
 }
--- src/layouts/AuthorizedLayout/AuthorizedLayout.js
+++ src/layouts/AuthorizedLayout/AuthorizedLayout.js
@@ -1,12 +1,13 @@
+import { Box } from "@mui/system";
 import React from "react";
 
 import { Navbar } from "../../components/Navbar/Navbar";
 
 export const AuthorizedLayout = ({ children }) => {
   return (
-    <>
+    <Box sx={{ p: 3 }}>
       <Navbar />
       {children}
-    </>
+    </Box>
   );
 };
