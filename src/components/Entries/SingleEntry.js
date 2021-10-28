5d6d6a07796a81da57d018b58023fa32205c73f5
ttzv-training-repos <teyed1fy@gmail.com>
2021-10-28 12:59:59 +0200

    change SingleEntry to use form, basic submitting without validation

--- src/components/Entries/SingleEntry.js
+++ src/components/Entries/SingleEntry.js
@@ -16,11 +16,12 @@ import useDeleteEntry from "../../mutations/useDeleteEntry";
 import useUpdateEntry from "../../mutations/useUpdateEntry";
 import { currentTime } from "../../utils/dateUtils";
 import React from "react";
+import { Form, Formik } from "formik";
 
 const SingleEntry = ({ entryData, tagBundleOptions, newEntryHandler }) => {
   const initialValues = {
     startTime: entryData.startTime,
-    endTime: entryData.endTime,
+    endTime: entryData.endTime ? entryData.endTime : "",
     tagBundleName: entryData.tag ? entryData.tag.tagBundle.name : "",
     tagName: entryData.tag ? entryData.tag.name : "",
   };
@@ -36,17 +37,12 @@ const SingleEntry = ({ entryData, tagBundleOptions, newEntryHandler }) => {
     });
   };
 
-  const handleChange = (e) => {
-    const obj = {};
-    obj[e.target.name] = e.target.value;
-    setEntryValues({ ...entryValues, ...obj });
-  };
-
-  const handleSubmit = (e) => {
+  const handleSubmit = (formData) => {
+    console.log(formData);
     const currentEntryEndTime = currentTime();
     let newEntryStartTime;
-    const entryValObj = { ...entryValues };
-    if (!entryValues.endTime) {
+    const entryValObj = { ...formData };
+    if (entryValObj.endTime === "") {
       entryValObj.endTime = currentEntryEndTime;
     }
     newEntryStartTime = entryValObj.endTime;
@@ -54,66 +50,88 @@ const SingleEntry = ({ entryData, tagBundleOptions, newEntryHandler }) => {
     updateEntry({
       variables: { entryId: entryData._id, record: entryValObj },
     });
-    newEntryHandler(e, entryData.order, newEntryStartTime);
+    newEntryHandler(entryData.order, newEntryStartTime);
   };
 
   return (
-    <Stack
-      p={2}
-      spacing={2}
-      direction="row"
-      sx={{ border: "1px solid #cacaca", borderRadius: "1em" }}
+    <Formik
+      onSubmit={handleSubmit}
+      initialValues={initialValues}
+      isInitialValid={false}
     >
-      <br></br>
-      <TextField
-        label="Czas rozpoczęcia"
-        value={entryValues.startTime}
-        name={"startTime"}
-        onChange={handleChange}
-      ></TextField>
-      <TextField
-        label="Czas zakończenia"
-        value={entryValues.endTime}
-        name={"endTime"}
-        onChange={handleChange}
-      ></TextField>
-      <FormControl sx={{ minWidth: 150, maxWidth: 150 }}>
-        <InputLabel id={`tag-bundle-select-label-${entryData._id}`}>
-          Tag Bundle
-        </InputLabel>
-        <Select
-          labelId={`tag-bundle-select-label-${entryData._id}`}
-          id={`tag-bundle-select-${entryData._id}`}
-          label="Tag Bundle"
-          name={"tagBundle"}
-          value={entryValues.tagBundleName}
-          onChange={(e) => console.log(e.target.value)}
-        >
-          <MenuItem value="">Brak</MenuItem>
-          {tagBundleOptions?.map((name, index) => (
-            <MenuItem value={name} key={index}>
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <Autocomplete
-        freeSolo
-        disablePortal
-        id={`tag-field-${entryData._id}`}
-        options={["tagOptions"]}
-        onInput={(e) => console.log(e.target.value)}
-        name={"tag"}
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Tag" />}
-      />
-      <Button onClick={handleSubmit}>
-        <AddCircleIcon></AddCircleIcon>
-      </Button>
-      <Button onClick={deleteEntryHandler}>
-        <DeleteForeverIcon></DeleteForeverIcon>
-      </Button>
-    </Stack>
+      {({
+        handleChange,
+        handleBlur,
+        setValues,
+        values,
+        touched,
+        isValid,
+        errors,
+      }) => {
+        const handleAutocompleteChange = (e, v) => {
+          setValues({ ...values, tagName: v });
+        };
+        return (
+          <Form>
+            <Stack
+              p={2}
+              spacing={2}
+              direction="row"
+              sx={{ border: "1px solid #cacaca", borderRadius: "1em" }}
+            >
+              <TextField
+                type="text"
+                label="Czas rozpoczęcia"
+                name="startTime"
+                onChange={handleChange}
+                value={values.startTime}
+              ></TextField>
+              <TextField
+                type="text"
+                label="Czas zakończenia"
+                name="endTime"
+                onChange={handleChange}
+                value={
+                  values.endTime === "" ? entryValues.endTime : values.endTime
+                }
+              ></TextField>
+              <FormControl sx={{ minWidth: 150, maxWidth: 150 }}>
+                <InputLabel>Tag Bundle</InputLabel>
+                <Select
+                  label="Tag Bundle"
+                  name="tagBundleName"
+                  onChange={handleChange}
+                  value={values.tagBundleName}
+                >
+                  <MenuItem value="">Brak</MenuItem>
+                  {tagBundleOptions?.map((name, index) => (
+                    <MenuItem value={name} key={index} name="tagBundleName">
+                      {name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <Autocomplete
+                freeSolo
+                disablePortal
+                options={["tagOptions"]}
+                sx={{ width: 300 }}
+                name="tagName"
+                value={values.tagName}
+                onInputChange={(e, v) => handleAutocompleteChange(e, v)}
+                renderInput={(params) => <TextField {...params} label="Tag" />}
+              />
+              <Button type="submit">
+                <AddCircleIcon></AddCircleIcon>
+              </Button>
+              <Button onClick={deleteEntryHandler}>
+                <DeleteForeverIcon></DeleteForeverIcon>
+              </Button>
+            </Stack>
+          </Form>
+        );
+      }}
+    </Formik>
   );
 };
 
