daf5a570efb679c2c2d2d0f3cf96518cec700b54
Łukasz Garncarczyk <lgarncarczyk@inewi.pl>
2021-10-29 14:52:06 +0200

    fix auth layout

--- src/routing/AuthorizedApp.js
+++ src/routing/AuthorizedApp.js
@@ -1,3 +1,4 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Route, Switch } from "react-router";
 
@@ -10,15 +11,21 @@ import { PageNotFound } from "../pages/PageNotFound/PageNotFound";
 import { Settings } from "../pages/Settings/Settings";
 
 export const AuthorizedApp = () => {
+  const { isAuthenticated } = useAuth0();
+
   return (
-    <AuthorizedLayout>
-      <Switch>
-        <Route path={ROUTES.CALENDAR} component={Calendar} />
-        <Route exact path={ROUTES.BUNDLE} component={Bundle} />
-        <Route path={ROUTES.BUNDLE_ITEM} component={BundleItem} />
-        <Route path={ROUTES.SETTINGS} component={Settings} />
-        <Route exact path={ROUTES.NOT_FOUND} component={PageNotFound} />
-      </Switch>
-    </AuthorizedLayout>
+    <>
+      {isAuthenticated && (
+        <AuthorizedLayout>
+          <Switch>
+            <Route path={ROUTES.CALENDAR} component={Calendar} />
+            <Route exact path={ROUTES.BUNDLE} component={Bundle} />
+            <Route path={ROUTES.BUNDLE_ITEM} component={BundleItem} />
+            <Route path={ROUTES.SETTINGS} component={Settings} />
+            <Route exact path={ROUTES.NOT_FOUND} component={PageNotFound} />
+          </Switch>
+        </AuthorizedLayout>
+      )}
+    </>
   );
 };
